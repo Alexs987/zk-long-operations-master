d54b7440eacc8eaa824be5d32f4805ce0eaf7c2b
Alexs987 <104712558+Alexs987@users.noreply.github.com>
2023-02-20 19:03:02 -0600

    Update Test.java
    Se crea método hola

--- src/main/java/zk/example/longoperations/test/Test.java
+++ src/main/java/zk/example/longoperations/test/Test.java
@@ -1,7 +1,12 @@
 package zk.example.longoperations.test;
 
+public void hola(){
+	System.out.println("Clase de ejemplo!");
+}
+
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("Clase de ejemplo!");
+		Test test = new Test();
+		test.hola();
 	}
 }
